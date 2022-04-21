b93c6c0876dcbc0d06ade5ba566f445bced8e9ba
shiirudo@outlook.fr <shiirudo@outlook.fr>
2022-04-21 23:34:22 +0200

    Update imperial_to_metric.py
    - Use of functions for future reuse
    - Simplification of calculation (imperial to metric) by using less variables and making formula compact
    - Use of f-strings instead of unorganized mess
    - Welcome message
    - Selection menu useable for future new modes of convertion

--- imperial_to_metric.py
+++ imperial_to_metric.py
@@ -1,8 +1,21 @@
-feet = float(input("Number of feet: "))
-inches = float(input("Number of inches: "))
+def imperial_to_metric():
+    feet = int(input("Number of feet? "))
+    inches = int(input("Number of inches? "))
+    result = (feet*12 + inches)*2.54
 
-feet_in_inches = feet*12
-total_inches = feet_in_inches + inches
+    print(f"{feet}ft{inches} equals {result}cm")
+    
+print("Welcome to this convertion program!")
 
-print("Number of total inches: " + str(total_inches))
-print("Number of cm: " + str(total_inches*2.54))
+while True:
+    print("\n" + "Please select a convertion mode.")
+    print("    1. Convert imperial height (ft/in) to metric height (cm)" + "\n")
+    option = input("Select mode n°: ")
+    
+    if option == str(1):
+        imperial_to_metric()
+        break
+    elif option != str(1):
+        print("ERROR: Not an option")
+    
+
